7282a5de509b243afa7df2029b05b1f6672e6023
Walter Nwaugo <47696744+walterthesmart@users.noreply.github.com>
2024-06-09 16:51:38 +0100

    feat: Add lazy loading for counter component

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,9 +1,21 @@
-imp
+import { useReadContract } from "thirdweb/react";
+import {CONTRACT} from "../utils/constants";
+
+
 
 const Counter: React.FC = () => {
+    const {data: count, isLoading: loadingCount} = useReadContract({
+        contract: CONTRACT,
+        method: "getCount"
+    });
     return (
         <div style={{marginTop: "20px"}}>
             <h1>Counter</h1>
+            {loadingCount ? (
+                <h2>.............loading..........</h2>
+            ) : (
+                <h2>{count ?.toString()}</h2>
+            )}
         </div>
     )
 }
